21c10a58f3bb2c465d3e4d8b7be1a1b2baaee745
asrarhaque <asrarul97@gmail.com>
2024-03-03 06:39:14 +0530

    Revert "cookie config in localhost"
    This reverts commit dff467368605a1d17af73b79fb9755e870821e18.

--- parent/jukebox-config/src/main/java/com/cannizarro/jukebox/config/security/jwt/cookie/CookieBuilder.java
+++ parent/jukebox-config/src/main/java/com/cannizarro/jukebox/config/security/jwt/cookie/CookieBuilder.java
@@ -1,7 +1,29 @@
 package com.cannizarro.jukebox.config.security.jwt.cookie;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.Cookie;
 import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
 
-public interface CookieBuilder {
-    ResponseCookie getCookie(String key, String value);
+import java.time.Duration;
+
+@Component
+public class CookieBuilder {
+
+    @Value("${jwt.max_age_days}")
+    private int maxAgeInDays;
+
+    @Value("${server.domain}")
+    private String domain;
+
+    public ResponseCookie getCookie(String key, String value){
+        return ResponseCookie.from(key, value)
+                .httpOnly(true)
+                .maxAge(Duration.ofDays(maxAgeInDays))
+                .secure(true)
+                .path("/jukebox/")
+                .domain(domain)
+                .sameSite(Cookie.SameSite.NONE.attributeValue())
+                .build();
+    }
 }
